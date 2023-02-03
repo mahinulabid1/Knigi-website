c4ca332a39ef34788e5d9a3c9143e6a9d9016dda
himahinulabid@gmail.com <himahinulabid@gmailcom>
2023-02-03 22:08:52 +0600

    modifile_file

---
+++ pageComponent/_navigation.php
@@ -0,0 +1,134 @@
+<nav>
+    <div class=" flex">
+        <div class="dekstop-ham-container">
+            <div class="dekstop-ham-relative">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+        </div>
+        <div class="shop-btn">SHOP <i class="fas fa-chevron-down shop-icon-down"></i></div>
+    </div>
+
+    <div>
+        <a title="Home Page" class="nav-logo" href="index.html">Knigi</a>
+    </div>
+
+    <div>
+        <!-- search icon class" -->
+        <div class="search-icon"><i class="fas fa-search  search-icon-normal"></i> <i class="fas fa-times  search-icon-close"></i></div>
+        <div class="cart-btn cart-text-responsive">CART</div>
+        <div class="cart-btn cart-icon"><i class="fas fa-shopping-cart"></i></div>
+    </div>
+</nav>
+
+
+
+
+
+
+<!-- navigation menu hidden items -->
+<!-- START -->
+<div class="book-shop-separate">
+    <div class="bs-container">
+        <a href="./productDescription.html" class="b-s-item">
+            <div class="b-s-image-container">
+                <img class="center-middle" src="./app/img/product/individual/product1.jpg" alt="">
+                <div class="b-s-product-text-container center-middle">
+                    <p class="b-s-product-text">Woven Issue Four</p>
+                    <p class="b-s-product-text"><span class="discounted-regular-price">$18 USD</span><span class="discounted-price"> $9 USD</span> <span class="regular-price"></span></p>
+                </div>
+            </div>
+
+        </a>
+        <a href="./productDescription.html" class="b-s-item">
+            <div class="b-s-image-container">
+                <img class="center-middle" src="./app/img/product/individual/product2.jpg" alt="">
+                <div class="b-s-product-text-container center-middle">
+                    <p class="b-s-product-text">Earl of East London</p>
+                    <p class="b-s-product-text"><span class="discounted-regular-price"></span><span class="discounted-price"></span> <span class="regular-price"> $24 USD</span></p>
+                </div>
+            </div>
+
+        </a>
+        <a href="./productDescription.html" class="b-s-item">
+            <div class="b-s-image-container">
+                <img class="center-middle" src="./app/img/product/individual/product3.jpg" alt="">
+                <div class="b-s-product-text-container center-middle">
+                    <p class="b-s-product-text">Earl of East London</p>
+                    <p class="b-s-product-text"><span class="discounted-regular-price"></span><span class="discounted-price"></span> <span class="regular-price"> $14 USD</span></p>
+                </div>
+            </div>
+
+        </a>
+        <a href="./productDescription.html" class="b-s-item">
+            <div class="b-s-image-container">
+                <img class="center-middle" src="./app/img/product/individual/product4.jpg" alt="">
+                <div class="b-s-product-text-container center-middle">
+                    <p class="b-s-product-text">Earl of East London</p>
+                    <p class="b-s-product-text"><span class="discounted-regular-price">$24 USD</span><span class="discounted-price">$12 USD</span> <span class="regular-price"> </span></p>
+                </div>
+            </div>
+
+        </a>
+    </div>
+    <a class="shop-btn-shop-all-link transition" href="./shopall.html">SHOP ALL <i style="padding-left: 20px;" class="fas fa-chevron-right"></i></a>
+    <div class="empty-space-close">
+
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="search-display">
+    <div class="search-display-text-container center-middle">
+        <form action="" class="serach-box-form">
+            <div class="search-display-flex-container">
+                <input class="search-bar" type="text" placeholder="Search Woven + Hit Enter">
+                <button class="search-submit-btn"><i class="fas fa-chevron-right"></i></button>
+            </div>
+
+            <p class="search-close-indication">PRESS <span class="span-search-close">[ESC]</span> TO CLOSE</p>
+        </form>
+    </div>
+</div>
+
+<div class="cart-holder">
+    <div class="cart">
+        <div class="cart-flex-container">
+            <div class="cart-header-info">Your Cart <span class="cart-total-amout">$35 USD</span></div>
+            <div class="cross-btn"><i class="fas fa-times"></i></div>
+        </div>
+        <div class="items-in-cart-container">
+            <div class="items-in-cart">
+                <div class="items-cart-name">Woven Issue 4</div>
+                <div class="price-tag-cart">$ 20 USD</div>
+            </div>
+            <div class="items-in-cart">
+                <div class="items-cart-name">As you like it</div>
+                <div class="price-tag-cart">$ 15 USD</div>
+            </div>
+
+        </div>
+        <div class="proceed-to-checkout"><a class="proceedtoCheck-btn" href="">CHECKOUT <i class="fas fa-angle-double-right"></i></a></div>
+    </div>
+</div>
+
+
+<!-- js event div -->
+<div class="nav-in-ham">
+    <div class="nav-in-ham-content-container">
+        <a href="./allStories.html" class="nav-in-ham-content">STORIES</a>
+        <a href="./films.html" class="nav-in-ham-content">FILMS</a>
+        <a href="./shopall.html" class="nav-in-ham-content">SHOP</a>
+        <a href="./allBlogs.html" class="nav-in-ham-content">BLOG</a>
+        <a href="./contactUs.html" class="nav-in-ham-content">ABOUT</a>
+        <a href="./contactUs.html" class="request-interview-btn">REQUEST INTERVIEW</a>
+    </div>
+</div>
+<!-- end -->
