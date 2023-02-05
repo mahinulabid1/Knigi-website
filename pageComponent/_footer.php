196f009ebacc72436f63b9940f6fdebf8ec3ab65
Mahinul Mostafa Abid <mahinulmostafa@gmail.com>
2023-02-05 14:36:49 +0600

    modified footer page

--- pageComponent/_footer.php
+++ pageComponent/_footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <div class="footer-item"><a href="./collaborate.html">Collaborate With Us</a> </div>
-    <div class="footer-item"><a href="./contactUs.html">Contact Us</a> </div>
+    <div class="footer-item"><a href="./collaborate.php">Collaborate With Us</a> </div>
+    <div class="footer-item"><a href="./contactUs.php">Contact Us</a> </div>
     <div class="footer-item"><a href="">Stay In Touch!</a> </div>
-    <div class="footer-item"><a href="./FAQ.html">FAQ</a> </div>
-    <div class="footer-item"><a href="./termsandCondition.html">Terms & Conditions</a> </div>
+    <div class="footer-item"><a href="./FAQ.php">FAQ</a> </div>
+    <div class="footer-item"><a href="./termsandCondition.php">Terms & Conditions</a> </div>
     <div class="footer-item copyright-text">© WOVEN 2021</div>
 </footer>
